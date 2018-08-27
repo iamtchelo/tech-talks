7c3e4ef554f4af90d67bb4099f3f7bb8de32e53e
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-08-26 22:57:55 -0300

    Create graphql client project

---
+++ graphql-intro/client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => (
+  <section>
+    <h1>App</h1>
+  </section>
+)
+
+export default App;
---
+++ graphql-intro/client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const client = new ApolloClient({
+  uri: process.env.GRAPHQL_ENDPOINT
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
+registerServiceWorker();
