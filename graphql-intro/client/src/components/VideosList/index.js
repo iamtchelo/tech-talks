eda6044d3ce4a24432933f4a477edb63c6391625
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-08-27 20:54:51 -0300

    Create <VideosList /> component

---
+++ graphql-intro/client/src/components/VideosList/index.js
@@ -0,0 +1,3 @@
+import VideosList from './videosList';
+
+export default VideosList;
---
+++ graphql-intro/client/src/components/VideosList/videosList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'react-emotion';
+import PlayIcon from '../../assets/img/player.svg';
+
+const Wrapper = styled('section')`
+`;
+
+const VideosWrapper = styled('section')`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+const Title = styled('h1')`
+  display: block;
+  padding: 10px 0;
+  border-bottom: 1px solid #CCC;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  color: #4E0250;
+`
+
+const Video = styled('div')`
+  position: relative;
+  padding: 10px;
+  margin: 10px 8px;
+  cursor: pointer;
+  background: white;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+
+  &:hover::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: 40px;
+    background-image: url(${PlayIcon});
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-color: rgba(255,255,255,0.7);
+  }
+`;
+
+const VideoImage = styled('img')`
+`;
+
+const VideosList = () => (
+  <Wrapper>
+    <Title>Youtube</Title>
+    <VideosWrapper>
+      {/* videos here */}
+    </VideosWrapper>
+  </Wrapper>
+);
+
+export default VideosList;
