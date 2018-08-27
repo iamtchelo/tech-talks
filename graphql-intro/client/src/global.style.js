1894d2ddbd7970585c01c205f2ad06b24930d8e2
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-08-27 00:16:35 -0300

    Add global styles

---
+++ graphql-intro/client/src/global.style.js
@@ -0,0 +1,14 @@
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body, html {
+    background: #F4F4F4;
+    color: #333;
+    font: 16px Arial, Helvetica, sans-serif;
+  }
+`;
