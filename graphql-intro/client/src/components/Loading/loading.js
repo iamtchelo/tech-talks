5de76801db06bd5bcae189ce857a698f98d1fff2
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-08-27 21:58:47 -0300

    Create <Loading /> component

---
+++ graphql-intro/client/src/components/Loading/loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'react-emotion';
+import LoadingGIF from '../../assets/img/loading.svg';
+
+const LoadingWrapper = styled('section')`
+  display: flex;
+  justify-content: center;
+  height: calc(100vh - 170px);
+`;
+
+const LoadingStyled = styled('img')``;
+
+const Loading = () => (
+  <LoadingWrapper>
+    <LoadingStyled
+      src={LoadingGIF}
+      width={100} />
+  </LoadingWrapper>
+);
+
+export default Loading;
